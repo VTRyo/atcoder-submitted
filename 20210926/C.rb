249bce4360d99a9ffd094f494d1d6b1945aacc22
Ryo inaba <vtryo.work@gmail.com>
2021-09-27 21:40:33 +0900

    Answers after the tournament

--- 20210926/C.rb
+++ 20210926/C.rb
@@ -1,21 +1,17 @@
 n = gets.to_i
 array = gets.split.map(&:to_i)
 x = gets.to_i
-array_sum = array.sum
-sum_num = (x / array_sum)
+array_sum = array.sum # 配列要素の合計
+result = (x / array_sum) * n # xを配列要素の合計で割って配列要素数でかけることで、すくなくとも何個分の要素が使われるかが判明する
+r = x % array_sum
 
-# すくなくともこの回数以上のところに答えがある
-res_num =  sum_num * n
-
-sum = 0
-sum += (array*sum_num).size # 項目回数
-array_sum_n = (array*sum_num).sum # 連結内部合計
-
-for i in array_sum_n..x do
-  if x > array_sum_n
-    array_sum_n += array[i]
-  end
+while r >= 0 do
+  result += 1
+  r -= array.shift # 配列から要素を削除しつつ、rの数値もマイナスする。0を下回った段階で、和が初めてxを超えたことになるので停止
 end
+puts result
+
+
 
 # 時間切れにつき未得点。途中Commitしておく
 
